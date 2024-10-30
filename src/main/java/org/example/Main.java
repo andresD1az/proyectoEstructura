965f51ca8a33166176bce8c0d3779ad3daf4c868
andresD1az <131607135+andresD1az@users.noreply.github.com>
2024-10-30 07:59:25 -0500

    nuevo repositorio de estructuras

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello  Estructura project");
+        System.out.println("nuevo  Estructura project");
     }
 }
